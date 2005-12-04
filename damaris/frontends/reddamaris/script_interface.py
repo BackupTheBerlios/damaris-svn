0717d05cbf206bf50a3919a4b2c14075f2f0f3e3
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2005-12-04 11:45:29 +0000

    class for all experiment threads, dump extended by script data

--- damaris/frontends/reddamaris/script_interface.py
+++ damaris/frontends/reddamaris/script_interface.py
@@ -3,6 +3,8 @@
 import time
 import sys
 import os
+import os.path
+import tables
 import DataPool
 import ResultReader
 import ExperimentWriter
@@ -20,6 +22,106 @@ def some_listener(event):
             ratio=100.0
         print "\r%d/%d (%.0f%%)"%(r,e,ratio),
 
+
+class ScriptInterface:
+
+    def __init__(self, exp_script=None, res_script=None, backend_executable=None, spool_dir="spool"):
+        self.exp_script=exp_script
+        self.res_script=res_script
+        self.backend_executable=backend_executable
+        self.spool_dir=os.path.abspath(spool_dir)
+
+        self.exp_writer=self.res_reader=self.back_driver=None
+        if self.backend_executable is not None:
+            self.back_driver=BackendDriver.BackendDriver(self.backend_executable, spool_dir)
+            if self.exp_script: self.exp_writer=self.back_driver.get_exp_writer()
+            if self.res_script: self.res_reader=self.back_driver.get_res_reader()
+        else:
+            self.back_driver=None
+            if self.exp_script: self.exp_writer=ExperimentWriter.ExperimentWriter(spool_dir)
+            if self.res_script: self.res_reader=ResultReader.ResultReader(spool_dir)
+
+        self.data=DataPool.DataPool()
+
+
+    def runScripts(self):
+        # get script engines
+        if self.exp_script and self.exp_writer:
+            self.exp_handling=ExperimentHandling.ExperimentHandling(self.exp_script, self.exp_writer, self.data)
+        if self.res_script and self.res_reader:
+            self.res_handling=ResultHandling.ResultHandling(self.res_script, self.res_reader, self.data)
+
+        # start them
+        if self.exp_handling: self.exp_handling.start()
+        if self.back_driver is not None: self.back_driver.start()
+        if self.res_handling: self.res_handling.start()
+
+    def waitForScriptsEnding(self):
+        # time of last dump
+        dump_interval=600
+        next_dump_time=time.time()+dump_interval
+        # keyboard interrupts are handled in extra cleanup loop
+        try:
+            while filter(None,[self.exp_handling,self.res_handling,self.back_driver]):
+                time.sleep(0.1)
+                if time.time()>next_dump_time:
+                    self.dump_data("data_pool.h5")
+                    next_dump_time+=dump_interval
+
+                if self.exp_handling is not None:
+                    if not self.exp_handling.isAlive():
+                        self.exp_handling.join()
+                        if self.exp_handling.raised_exception:
+                            print ": experiment script failed at line %d (function %s): %s"%(self.exp_handling.location[0],
+                                                                                             self.exp_handling.location[1],
+                                                                                             self.exp_handling.raised_exception)
+                        else:
+                            print ": experiment script finished"
+                        self.exp_handling = None
+
+                if self.res_handling is not None:
+                    if not self.res_handling.isAlive():
+                        self.res_handling.join()
+                        if self.res_handling.raised_exception:
+                            print ": result script failed at line %d (function %s): %s"%(self.res_handling.location[0],
+                                                                                         self.res_handling.location[1],
+                                                                                         self.res_handling.raised_exception)
+                        else:
+                            print ": result script finished"
+                        self.res_handling = None
+
+                if self.back_driver is not None:
+                    if not self.back_driver.isAlive():
+                        print ": backend finished"
+                        self.back_driver=None
+
+        except KeyboardInterrupt:
+            still_running=filter(None,[self.exp_handling,self.res_handling,self.back_driver])
+            for r in still_running:
+                r.quit_flag.set()
+
+            for r in still_running:
+                r.join()
+
+    def dump_data(self, filename):
+        try:
+            # write data from pool
+            self.data.dump_hdf5(filename)
+            # write scripts
+            dump_file=tables.openFile(filename,"r+")
+            scriptgroup=dump_file.createGroup("/","scripts","Used Scripts")
+            dump_file.createArray(scriptgroup,"experiment_script", self.exp_script)
+            dump_file.createArray(scriptgroup,"result_script", self.res_script)
+            dump_file.createArray(scriptgroup,"backend_executable", self.backend_executable)
+            dump_file.createArray(scriptgroup,"spool_directory", self.spool_dir)
+            dump_file.flush()
+            dump_file=None
+            # todo
+        except Exception,e:
+            print "dump failed", e
+       
+
+
 if __name__=="__main__":
 
     if len(sys.argv)==1:
@@ -29,72 +131,19 @@ if __name__=="__main__":
         spool_dir=os.getcwd()
     else:
         spool_dir=sys.argv[2]
-
-    if True:
-        bd=BackendDriver.BackendDriver("/home/achim/damaris/backends/machines/Mobilecore.exe", spool_dir)
-        exp_writer=bd.get_exp_writer()
-        res_reader=bd.get_res_reader()
-    else:
-        bd=None
-        exp_writer=ExperimentWriter.ExperimentWriter(spool_dir)
-        res_reader=ResultReader.ResultReader(spool_dir)
     
     scriptfile=open(sys.argv[1])
     script=scriptfile.read()
     scriptfile=None
-    data=DataPool.DataPool()
-    data.register_listener(some_listener)
-    exp=ExperimentHandling.ExperimentHandling(script, exp_writer, data)
-    res=ResultHandling.ResultHandling(script, res_reader, data)
-    exp.start()
-    if bd is not None: bd.start()
-    res.start()
 
-    # time of last dump
-    dump_interval=600
-    next_dump_time=time.time()+dump_interval
-    # keyboard interrupts are handled in extra cleanup loop
-    try:
-        while filter(None,[exp,res,bd]):
-            time.sleep(0.1)
-            if time.time()>next_dump_time:
-                try:
-                    data.dump_hdf5("data_pool.h5")
-                except Exception,e:
-                    print "dump failed", e
-                next_dump_time+=dump_interval
-            
-            if exp is not None:
-                if not exp.isAlive():
-                    exp.join()
-                    if exp.raised_exception:
-                        print ": experiment script failed at line %d (function %s): %s"%(exp.location[0],exp.location[1],exp.raised_exception)
-                    else:
-                        print ": experiment script finished"
-                    exp = None
+    si=ScriptInterface(script, script,"/home/achim/damaris/backends/machines/Mobilecore.exe", spool_dir)
 
-            if res is not None:
-                if not res.isAlive():
-                    res.join()
-                    if res.raised_exception:
-                        print ": result script failed at line %d (function %s): %s"%(res.location[0],res.location[1],res.raised_exception)
-                    else:
-                        print ": result script finished"
-                    res = None
+    si.data.register_listener(some_listener)
 
-            if bd is not None:
-                if not bd.isAlive():
-                    print ": backend finished"
-                    bd=None
+    si.runScripts()
 
-    except KeyboardInterrupt:
-        still_running=filter(None,[exp,res,bd])
-        for r in still_running:
-            r.quit_flag.set()
+    si.waitForScriptsEnding()
 
-        for r in still_running:
-            r.join()
+    si.dump_data("data_pool.h5")
 
-    # dump data pool
-    data.dump_hdf5("data_pool.h5")
-    data=None
+    si=None
