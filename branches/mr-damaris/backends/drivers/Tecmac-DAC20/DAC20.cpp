238db449328e4dafe5161429b334ea613cb1b33b
markusro <markusro@b231d36d-50f4-0310-9822-a9ec2037227d>
2006-02-21 12:40:26 +0000

    Bug behoben, Clean-Up

--- branches/mr-damaris/backends/drivers/Tecmac-DAC20/DAC20.cpp
+++ branches/mr-damaris/backends/drivers/Tecmac-DAC20/DAC20.cpp
@@ -9,9 +9,9 @@
 #define DAC_BIT_DEPTH 20
 
 // The channel configuration
-#define DATA_BIT 2//18	
-#define CLK_BIT 0//16
-#define LE_BIT 1//17
+#define DATA_BIT 18//18	
+#define CLK_BIT 16//16
+#define LE_BIT 17//17
 
 PFG::PFG(int myid): id(myid) {
 	dac_value = 0;
@@ -37,26 +37,26 @@ void PFG::set_dac(state& experiment) {
 //		std::cout << "first state"<< std::endl;
 		// Initialize the DAC to "0"
 		state s(9e-8);
-		ttlout le;
-		le.id=0;
-		le.ttls=int(pow(2.0, LE_BIT));
-		s.push_front(&le);
+		ttlout* latch=new ttlout();
+		latch->id=0;
+		latch->ttls = 1 << LE_BIT;
+		s.push_front(latch);
 		state::iterator my_state_iterator = exp_sequence->begin();
 		for ( int i = 0; i < DAC_BIT_DEPTH; i++ ) {
-		    le.ttls=int(pow(2.0, LE_BIT));
+		    latch->ttls=1 << LE_BIT;
 		    exp_sequence->insert(my_state_iterator, s.copy_new());
-		    le.ttls=int(pow(2.0, LE_BIT))+int(pow(2.0,CLK_BIT));
+		    latch->ttls=(1 << LE_BIT) + (1 << CLK_BIT);
 		    exp_sequence->insert(my_state_iterator, s.copy_new());
 		    if ( i == DAC_BIT_DEPTH-1 ) {
 			//read in the word (41st pulse)
-			le.ttls=0;
+			latch->ttls=0;
 			exp_sequence->insert(my_state_iterator, s.copy_new());
 		    }
 		}
 		// 42nd pulse
 		// the state should be 2ms long
 		s.length = 2e-3-41*9e-8;
-		le.ttls=int(pow(2.0,LE_BIT));
+		latch->ttls = 1 << LE_BIT;
 		exp_sequence->insert(my_state_iterator, s.copy_new());
 	}
 }
@@ -131,13 +131,13 @@ void PFG::set_dac_recursive(state_sequent& the_sequence, state::iterator& the_st
 				// except for the last bit
 				// ugly: reverse the bit pattern
 				for (int i = DAC_BIT_DEPTH-1; i >= 0; i--) {	
-					register_ttls->ttls = (int(pow(2.0, DATA_BIT))*dac_word[i] + int(pow(2.0, LE_BIT)));
+					register_ttls->ttls = (1 << DATA_BIT)*dac_word[i] + (1 << LE_BIT);
 					the_sequence.insert(the_state,register_state->copy_new());
-					register_ttls->ttls = (int(pow(2.0, DATA_BIT))*dac_word[i] + int(pow(2.0, CLK_BIT)) + int(pow(2.0, LE_BIT)));
+					register_ttls->ttls = (1 << DATA_BIT )*dac_word[i] + (1 << CLK_BIT) + (1 << LE_BIT);
 					the_sequence.insert(the_state,register_state->copy_new());
 					//std::cout << dac_word[i];
 					if (i == 0 /*(DAC_BIT_DEPTH-1)*/) {// last bit => LE low, tell DAC to read the word in 
-						register_ttls->ttls = 0; //  int(pow(2.0, DATA_BIT))*bit;
+						register_ttls->ttls = 0; //  1<< DATA_BIT*bit;
 						the_sequence.insert(the_state,register_state->copy_new());
 					}
 				}
@@ -147,7 +147,7 @@ void PFG::set_dac_recursive(state_sequent& the_sequence, state::iterator& the_st
 				ttlout* ttls=new ttlout();
 				// 42nd pulse
 				this_state->length -= 9e-8*41;
-				ttls->ttls = int(pow(2.0, LE_BIT));
+				ttls->ttls = 1 << LE_BIT;
 				this_state->push_front(ttls);
 				delete register_state;
 				delete PFG_aout;	
@@ -156,7 +156,7 @@ void PFG::set_dac_recursive(state_sequent& the_sequence, state::iterator& the_st
 		}
 		else {
 			ttlout* le_ttls=new ttlout();
-			le_ttls->ttls = int(pow(2.0, LE_BIT));
+			le_ttls->ttls = 1 << LE_BIT;
 			this_state->push_back(le_ttls);
 		}
 		// end of state modifications 
