c7196d2b81af06470bfbdaaa8feb67bf2ab74216
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2006-11-28 15:16:59 +0000

    synchronization supported

--- damaris/backends/machines/deuteron_backend.cpp
+++ damaris/backends/machines/deuteron_backend.cpp
@@ -10,10 +10,10 @@
 #include "drivers/SpinCore-PulseBlasterDDSIII/SpinCore-PulseBlasterDDSIII.h"
 
 /**
-   \defgroup magnexgradmachine Magnex Static Gradient NMR Spectrometer
+   \defgroup deuteronmachine Magnex Static Gradient NMR Spectrometer
    \ingroup machines
-   Uses Spincore Pulseblaster 24 Bit and Spectrum MI4021 together with PTS phase and frequency cable driver
-   Also implements Eurotherm temperature control
+   Uses Spincore Pulseblaster DDSIII and Spectrum MI4021
+
 
    \par Starting the hardware
    Switch on the amplifier
@@ -24,50 +24,85 @@
    line 0 for gate
    line 1 for pulse
    line 2 for trigger
-   line 3 free
+   line 3 for sync active
  */
-class magnexgrad_hardware: public hardware {
+class deuteron_hardware: public hardware {
+
+  SpinCorePulseBlasterDDSIII* dds;
+  SpectrumMI40xxSeries* my_adc;
+
 
 public:
-  magnexgrad_hardware(){
+ deuteron_hardware(){
       ttlout trigger;
       trigger.id=0;
       trigger.ttls=4; /* line 2 */
-      the_adc=new SpectrumMI40xxSeries(trigger);
-      SpinCorePulseBlasterDDSIII* dds=new SpinCorePulseBlasterDDSIII();
+      my_adc=new SpectrumMI40xxSeries(trigger);
+      dds=new SpinCorePulseBlasterDDSIII(0,     // ttlout id
+					 100e6, //clock
+					 0x8);  // sync active: line 3
+      
+      the_adc=my_adc;
       the_pg=dds;
       the_fg=dds;
       the_tc=NULL;
 
-      //configurable_devices["T"]=the_tc;
   }
 
-
-  /**
-     print out a temperature line
-   */
   virtual result* experiment(const state& exp) {
-    result* r=hardware::experiment(exp);
+    result* r=NULL;
+    for(size_t tries=0; r==NULL && core::term_signal==0 &&  tries<102; ++tries) {
+      state* work_copy=exp.copy_flat();
+      if (work_copy==NULL) return new error_result(1,"could create work copy of experiment sequence");
+      try {
+	if (the_fg!=NULL)
+	  the_fg->set_frequency(*work_copy);
+	if (the_adc!=NULL)
+	  the_adc->set_daq(*work_copy);
+	// the pulse generator is necessary
+	dds->run_pulse_program_w_sync(*work_copy, my_adc->get_sample_clock_frequency());
+	// wait for pulse generator
+	the_pg->wait_till_end();
+	// after that, the result must be available
+	if (the_adc!=NULL)
+	  r=the_adc->get_samples();
+	else
+	  r=new adc_result(1,0,NULL);
+      }
+      catch (frequ_exception e) {
+	r=new error_result(1,"frequ_exception: "+e);
+      }
+      catch (ADC_exception e) {
+	if (e!="ran into timeout!" || tries>=100)
+	  r=new error_result(1,"ADC_exception: "+e);
+      }
+      catch (pulse_exception p) {
+	r=new error_result(1,"pulse_exception: "+p);
+      }
+      delete work_copy;
+      if (core::quit_signal!=0) break;
+    }
     return r;
   }
 
-  virtual ~magnexgrad_hardware() {
-    if (the_adc!=NULL) delete the_adc;
-    if (the_pg!=NULL) delete the_pg;
+
+  virtual ~deuteron_hardware() {
+    if (the_adc!=NULL) {delete the_adc; the_adc=my_adc=NULL;}
+    if (the_pg!=NULL) {delete the_pg; the_pg=dds=NULL;}
     if (the_tc!=NULL) delete the_tc;
   }
 
 };
 
 /**
-   \brief brings standard core together with the Mobile NMR hardware
+   \brief brings standard core together with the deuteron spectrometer hardware
 */
-class magnexgrad_core: public core {
+class deuteron_core: public core {
   std::string the_name;
 public:
-    magnexgrad_core(const core_config& conf): core(conf) {
-	the_hardware=new magnexgrad_hardware();
-	the_name="magnexgrad";
+    deuteron_core(const core_config& conf): core(conf) {
+      the_hardware=new deuteron_hardware();
+      the_name="deuteron";
   }
   virtual const std::string& core_name() const {
   	return the_name;
@@ -83,7 +118,7 @@ int main(int argc, const char** argv) {
   try {
       core_config my_conf(argv, argc);
       // setup input and output
-      magnexgrad_core my_core(my_conf);
+      deuteron_core my_core(my_conf);
       // start core application
       my_core.run();
   }
