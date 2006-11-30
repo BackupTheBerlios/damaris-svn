7a89e0ed06317130e1be8c35e444b9646dd055a8
markusro <markusro@b231d36d-50f4-0310-9822-a9ec2037227d>
2006-11-30 12:47:29 +0000

    New core with synchronization

--- damaris/backends/machines/PFGcore.cpp
+++ damaris/backends/machines/PFGcore.cpp
@@ -29,29 +29,74 @@
 /**
    line 0 for gate
    line 1 for pulse
-   line 2 for trigger
+   line 22 for trigger
    line 3 free
  */
 class PFG_hardware: public hardware {
+
+SpinCorePulseBlaster24Bit* my_pulseblaster;
+  SpectrumMI40xxSeries* my_adc;
 
 public:
   PFG_hardware(){
       ttlout trigger;
       trigger.id=0;
-      trigger.ttls=4; /* line 2 */// 
-      the_adc=new SpectrumMI40xxSeries(trigger);
-      the_pg=new SpinCorePulseBlaster24Bit();
+      trigger.ttls=0x400000; /* line 22 */// 
+      my_adc=new SpectrumMI40xxSeries(trigger);
+      my_pulseblaster=new SpinCorePulseBlaster24Bit(0,1e8,0x800000);
       PTS* my_pts=new PTS_latched(0); // ID of PTS_analogout 0
       the_fg=my_pts;
+      the_pg=my_pulseblaster;
+      the_adc=my_adc;
       PFG* my_pfg=new PFG(1);
       the_gradientpg=my_pfg;
   }
 
+  result* PFG_hardware::experiment(const state& exp) {
+    result* r=NULL;
+    for(size_t tries=0; r==NULL && core::term_signal==0 &&  tries<102; ++tries) {
+      state* work_copy=exp.copy_flat();
+      if (work_copy==NULL) return new error_result(1,"could create work copy of experiment sequence");
+      try {
+	if (the_fg!=NULL)
+	  the_fg->set_frequency(*work_copy);
+	if (the_adc!=NULL)
+	  the_adc->set_daq(*work_copy);
+	if (the_gradientpg!=NULL)
+	  the_gradientpg->set_dac(*work_copy);
+	// the pulse generator is necessary
+	my_pulseblaster->run_pulse_program_w_sync(*work_copy, my_adc->get_sample_clock_frequency());
+	// wait for pulse generator
+	the_pg->wait_till_end();
+	// after that, the result must be available
+	if (the_adc!=NULL)
+	  r=the_adc->get_samples();
+	else
+	  r=new adc_result(1,0,NULL);
+      }
+      catch (frequ_exception e) {
+	r=new error_result(1,"frequ_exception: "+e);
+      }
+      catch (ADC_exception e) {
+	if (e!="ran into timeout!" || tries>=100)
+	  r=new error_result(1,"ADC_exception: "+e);
+      }
+      catch (pulse_exception p) {
+	r=new error_result(1,"pulse_exception: "+p);
+      }
+      delete work_copy;
+      if (core::quit_signal!=0) break;
+    }
+    return r;
+  }
+
+
   virtual ~PFG_hardware() {
     if (the_adc!=NULL) delete the_adc;
     if (the_pg!=NULL) delete the_pg;
-	if (the_gradientpg!=NULL) delete the_gradientpg;
-  }
+    if (the_gradientpg!=NULL) delete the_gradientpg;
+    if (the_fg!=NULL) delete the_fg;
+   }
 
 };
 
