742eccc161e5d0490ccabeb2bc916d95d772bea1
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2007-04-19 08:09:05 +0000

    include files are generated from vendor's archive

--- damaris/backends/drivers/Spectrum-MI40xxSeries/include/spectrum.h
+++
@@ -1,7 +0,0 @@
-HEAD  SpcInitPCIBoards  (int16 *pnCount, int16 *pnPCIVersion);
-HEAD  SpcInitBoard      (int16 nNr, int16 nTyp);
-HEAD  SpcSetParam       (int16 nNr, int32 lReg, int32 lValue);
-HEAD  SpcGetParam       (int16 nNr, int32 lReg, int32 *plValue);
-HEAD  SpcGetData        (int16 nNr, int16 nCh, int32 lStart, int32 lLen, dataptr pvData);
-HEAD  SpcSetData        (int16 nNr, int16 nCh, int32 lStart, int32 lLen, dataptr pvData);
-HEAD  SpcGetVersionInfo (char *pszBuffer, int nBufferLen);
