d8966f6c89759b0d5fe8d74a0a3104f6cdb64d65
tacke <tacke@b231d36d-50f4-0310-9822-a9ec2037227d>
2007-08-31 10:41:38 +0000

    Remove synchronisation card handling
    All credits go to Achim.

--- damaris/backends/machines/Mobile_wo_sync_backend.cpp
+++ damaris/backends/machines/Mobile_wo_sync_backend.cpp
@@ -44,7 +44,7 @@ public:
       trigger.ttls=1<<17; /* line 2 */
       my_adc=new SpectrumMI40xxSeries(trigger);
       // device_id=0, clock=100MHz, sync_mask: Bit 16
-      my_pulseblaster=new SpinCorePulseBlaster24Bit(0,1e8,1<<16);
+      my_pulseblaster=new SpinCorePulseBlaster24Bit(0,1e8,0<<16);
       my_pts=new PTS_latched(0);
 
       // publish devices
@@ -53,43 +53,6 @@ public:
       the_fg=my_pts;
   }
 
-  result* experiment(const state& exp) {
-    result* r=NULL;
-    for(size_t tries=0; r==NULL && core::term_signal==0 &&  tries<102; ++tries) {
-      state* work_copy=exp.copy_flat();
-      if (work_copy==NULL) return new error_result(1,"could create work copy of experiment sequence");
-      try {
-	if (the_fg!=NULL)
-	  the_fg->set_frequency(*work_copy);
-	if (the_adc!=NULL)
-	  the_adc->set_daq(*work_copy);
-	if (the_gradientpg!=NULL)
-	  the_gradientpg->set_dac(*work_copy);
-	// the pulse generator is necessary
-	my_pulseblaster->run_pulse_program_w_sync(*work_copy, my_adc->get_sample_clock_frequency());
-	// wait for pulse generator
-	the_pg->wait_till_end();
-	// after that, the result must be available
-	if (the_adc!=NULL)
-	  r=the_adc->get_samples();
-	else
-	  r=new adc_result(1,0,NULL);
-      }
-      catch (frequ_exception e) {
-	r=new error_result(1,"frequ_exception: "+e);
-      }
-      catch (ADC_exception e) {
-	if (e!="ran into timeout!" || tries>=100)
-	  r=new error_result(1,"ADC_exception: "+e);
-      }
-      catch (pulse_exception p) {
-	r=new error_result(1,"pulse_exception: "+p);
-      }
-      delete work_copy;
-      if (core::quit_signal!=0) break;
-    }
-    return r;
-  }
 
   virtual ~Mobile_hardware() {
     if (the_adc!=NULL) delete the_adc;
@@ -107,7 +70,7 @@ class Mobile_core: public core {
 public:
     Mobile_core(const core_config& conf): core(conf) {
 	the_hardware=new Mobile_hardware();
-	the_name="Mobile core";
+	the_name="Mobile backend without synchronisation card";
   }
   virtual const std::string& core_name() const {
   	return the_name;
