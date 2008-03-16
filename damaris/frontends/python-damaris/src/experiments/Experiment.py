a1f5ecfb5f865aff6881705684a38d4b07a90f20
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2008-03-16 22:58:01 +0000

    temporarily deleted changed checked in again (sorry)

--- damaris/frontends/python-damaris/src/experiments/Experiment.py
+++ damaris/frontends/python-damaris/src/experiments/Experiment.py
@@ -1,5 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 import types
+import numpy as N
 
 class StateBase(object):
 	def __init__(self):
@@ -276,7 +277,66 @@ class Quit(Experiment):
     # /Public Methods ------------------------------------------------------------------------------
 
 
-def lin_range(start,stop, step):
+# These methods return lists
+
+lin_range = N.arange
+
+def log_range(start, stop, stepno):
+	if (start<=0 or stop<=0 or stepno<1):
+		raise ValueError("start, stop must be positive and stepno must be >=1")
+	return N.logspace(N.log10(start),N.log10(stop), num=stepno)
+
+
+def staggered_range(some_range, size=3):
+	m=0
+	if isinstance(some_range, N.ndarray):
+		print "da"
+		is_numpy = True
+		some_range = list(some_range)
+	new_list=[]
+	for k in xrange(len(some_range)):
+		for i in xrange(size):
+			try:
+				index = (m*size)
+				new_list.append(some_range.pop(index))
+			except IndexError:
+				break
+		m+=1
+	if is_numpy:
+		new_list = N.asarray(new_list+some_range)
+	else:
+		new_list+=some_range
+	return new_list
+		
+
+def combined_ranges(*ranges):
+    new_list = []
+    for r in ranges:
+        new_list+=r
+    return new_list
+
+combine_ranges=combined_ranges
+
+def interleaved_range(some_list, left_out):
+	"""
+	in first run, do every n-th, then do n-1-th of the remaining values and so on...
+	"""
+	m=0
+	new_list = []
+	for j in xrange(left_out):
+		for i in xrange(len(some_list)):
+			if (i*left_out+m) < len(some_list):
+				new_list.append(some_list[i*left_out+m])
+			else:
+				m+=1
+				break
+	if isinstance(some_list, N.ndarray):
+		new_list = N.array(new_list) 
+	return new_list
+
+
+# These are the generators
+def lin_range_iter(start,stop, step):
     this_one=float(start)+0.0
     if step>0:
         while (this_one<=float(stop)):
@@ -288,7 +348,7 @@ def lin_range(start,stop, step):
             this_one+=float(step)
         
 
-def log_range(start, stop, stepno):
+def log_range_iter(start, stop, stepno):
     if (start<=0 or stop<=0 or stepno<1):
         raise ValueError("start, stop must be positive and stepno must be >=1")
     if int(stepno)==1:
@@ -298,8 +358,12 @@ def log_range(start, stop, stepno):
     for i in xrange(int(stepno)):
         yield start*(factor**i)
 
-def staggered_range(some_range, size = 1):
-    # do one, drop one
+def staggered_range_iter(some_range, size = 1):
+    """
+    size=1: do one, drop one, ....
+    size=n: do 1 ... n, drop n+1 ... 2*n
+    in a second run the dropped values were done
+    """
     left_out=[]
     try:
         while True:
@@ -314,12 +378,15 @@ def staggered_range(some_range, size = 1):
     for i in left_out:
         yield i
 
-
-def combine_ranges(*ranges):
+def combined_ranges_iter(*ranges):
+    """
+    iterate over one range after the other
+    """
     for r in ranges:
         for i in r:
             yield i
 
+combine_ranges_iter=combined_ranges_iter
 
 def self_test():
 	e = Experiment()
