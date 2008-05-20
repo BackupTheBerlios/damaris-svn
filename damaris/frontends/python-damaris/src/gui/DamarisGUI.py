0f9cc076595770ebcb9c2c9880f24b7c05262551
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2008-05-20 17:02:07 +0000

    updated versioning and cleanup in debian/rules

--- damaris/frontends/python-damaris/src/gui/DamarisGUI.py
+++ damaris/frontends/python-damaris/src/gui/DamarisGUI.py
@@ -71,7 +71,7 @@ from damaris.data import DataPool, Accumulation, ADC_Result, MeasurementResult
 debug=False
 
 # version info
-__version__="0.11-rc"
+__version__="0.11"
 
 class logstream:
     gui_log=None
