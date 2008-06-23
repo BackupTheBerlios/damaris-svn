ee92d12fb52aa791932dcbb432c4e5ea27dc8258
achim <achim@b231d36d-50f4-0310-9822-a9ec2037227d>
2008-06-23 16:51:10 +0000

    adapted driver load/unload script to udev support

--- damaris/backends/tools/nmr_drivers.sh
+++ damaris/backends/tools/nmr_drivers.sh
@@ -18,131 +18,99 @@ NMR_MODULE_DIR=/lib/modules/`uname -r`/kernel/damaris
 NMR_GROUP="nmr"
 
 # tools
-LSMOD=/sbin/lsmod
 RMMOD=/sbin/rmmod
-INSMOD=/sbin/insmod
-LSPCI=/bin/lspci
+MODPROBE=/sbin/modprobe
 KERNELCONF=/boot/config-`uname -r`
 
 # spectrum related stuff
+SPECTRUM_NAME=spc
 CONFIG_SMP="`grep 'CONFIG_SMP *=' $KERNELCONF | sed 's/.*=//'`"
 if test "x$CONFIG_SMP" = "xy"; then
   SPECTRUM_SMP_EXT="_smp";
 else
   SPECTRUM_SMP_EXT="";
 fi
-SPECTRUM_DEV="/dev/spc0"
-SPECTRUM_MAJOR=""
-SPECTRUM_MINOR=""
-SPECTRUM_MOD=$NMR_MODULE_DIR/spc$SPECTRUM_SMP_EXT.ko
+SPECTRUM_DEV=/dev/${SPECTRUM_NAME}0
+SPECTRUM_MOD_FILE=$NMR_MODULE_DIR/${SPECTRUM_NAME}${SPECTRUM_SMP_EXT}.ko
 
 # pulseblaster related stuff
-PULSEBLASTER_MOD=$NMR_MODULE_DIR/pulseblaster.ko
-PULSEBLASTER_IOPORT=""
-PULSEBLASTER_DEV="/dev/pulseblaster"
-PULSEBLASTER_MAJOR=""
-PULSEBLASTER_MINOR=""
-
-pb_findio() {
-	$LSPCI -vn |
-	     while read line; do
-		if echo "$line"|grep "ff00: 10e8:8852$" >/dev/null; then
-		    read line
-		    read line
-		    echo "$line"|sed 's/I\/O ports at \([0-9a-f]*\).*/0x\1/'
-		    break
-		fi
-	     done
-}
-
-find_major() {
- grep " $1\$" /proc/devices |sed "s/ *\([0-9]*\) $1/\1/"
-}
-
-find_minor_misc() {
- grep " $1\$" /proc/misc |sed "s/ *\([0-9]*\) $1/\1/"
-}
-
+PULSEBLASTER_NAME=pulseblaster
+PULSEBLASTER_MOD_FILE=$NMR_MODULE_DIR/$PULSEBLASTER_NAME.ko
+PULSEBLASTER_DEV=/dev/$PULSEBLASTER_NAME
 
 start() {
-	if $LSMOD | egrep "^spc|^pulseblaster">/dev/null ; then
-	   printf "nothing done, pulseblaster or spc already loaded...\n"
-	   return 
-	fi
-	if test \! \( -f $SPECTRUM_MOD -a -f $PULSEBLASTER_MOD \); then
-	    printf "pulseblaster or specturm module not available...\n"
-	    RETVAL=0
-	    return
-	fi
-	PULSEBLASTER_IOPORT=`pb_findio`
-	if test -z "$PULSEBLASTER_IOPORT"; then
-	    printf "pulseblaster IO ports not found...\n"
-	    RETVAL=$HARDWARE_FAIL_RETVAL
-	    return
-	fi
-	if ! $INSMOD $SPECTRUM_MOD >/dev/null 2>&1; then
-	    RETVAL=$HARDWARE_FAIL_RETVAL
-	    return
-       fi
-          
-	$INSMOD $PULSEBLASTER_MOD base_address=$PULSEBLASTER_IOPORT
 
-	# now find major device numbers
-	PULSEBLASTER_MAJOR=`find_major "pulseblaster"`
-        PULSEBLASTER_MINOR=0
-	if test -z "$PULSEBLASTER_MAJOR"; then
-	    printf "could not determine pulseblaster's major device number"
-	    RETVAL=2
-	    return
-	fi
-
-# 	SPECTRUM_MAJOR=`find_major "spec"`
-# 	if test -z "$SPECTRUM_MAJOR"; then
-# 	    # second chance: misc
-# 	    SPECTRUM_MAJOR=`find_major "misc"`
-# 	    SPECTRUM_MINOR=`find_minor_misc spc0`
-# 	    if test -z "$SPECTRUM_MINOR"; then
-# 		printf "could not determine spectrum's device numbers"
-# 		RETVAL=2
-# 		return
-# 	    fi
-# 	else
-# 	    SPECTRUM_MINOR=0
-# 	fi
-	
-	mknod $PULSEBLASTER_DEV c $PULSEBLASTER_MAJOR $PULSEBLASTER_MINOR
-#	mknod $SPECTRUM_DEV c $SPECTRUM_MAJOR $SPECTRUM_MINOR
-	if grep "^$NMR_GROUP:" /etc/group>/dev/null; then
-	    chgrp $NMR_GROUP $PULSEBLASTER_DEV
-	    chgrp $NMR_GROUP $SPECTRUM_DEV
-	    chmod g=rw,o= $PULSEBLASTER_DEV
-	    chmod g=rw,o= $SPECTRUM_DEV
+        SPECTRUM_RETVAL=0
+        # find out whether spectrum module is running
+	if egrep "^spc${SPECTRUM_SMP_EXT} " /proc/modules >/dev/null ; then
+	     echo "spectrum module already loaded"
 	else
-	    echo It is recommended to create a group named $NMR_GROUP
-	    chmod a=rw $PULSEBLASTER_DEV
-	    chmod a=rw $SPECTRUM_DEV
+	     # ok, load it
+             if test -f $SPECTRUM_MOD_FILE; then
+		if ! $MODPROBE ${SPECTRUM_NAME}${SPECTRUM_SMP_EXT} >/dev/null 2>&1; then
+		        echo "spectrum module failed to load"
+			SPECTRUM_RETVAL=1
+		fi
+	     else
+		echo "spectrum module not found"
+		return
+	     fi
+        fi
+
+        PULSEBLASTER_RETVAL=0
+        # find out whether pulseblaster module is running
+        if egrep "^pulseblaster " /proc/modules >/dev/null ; then
+	     echo "pulseblaster module already loaded"
+	else
+	     # ok, load it
+             if test -f $PULSEBLASTER_MOD_FILE; then
+		if ! $MODPROBE $PULSEBLASTER_NAME >/dev/null 2>&1; then
+			echo "pulseblaster module failed to load"
+			PULSEBLASTER_RETVAL=1
+		fi
+	     else
+		echo "pulseblaster module not found"
+		return
+	     fi
+        fi
+
+	# with udev, we should have links and also the rights set correctly
+        if test  $PULSEBLASTER_RETVAL -ne 0  -o $SPECTRUM_RETVAL -ne 0; then
+             return $HARDWARE_FAIL_RETVAL
+        fi
+
+	# have to wait for a short time, let udev do the work
+	/sbin/udevsettle
+	if ! test -c ${PULSEBLASTER_DEV}0; then
+		echo "no pulseblaster board is found, only the debug device at ${PULSEBLASTER_DEV}_debug is available"
 	fi
+
+
 }
 
 stop() {
-	if $LSMOD | egrep "^spc$SPECTRUM_SMP_EXT ">/dev/null ; then
-	  $RMMOD spc$SPECTRUM_SMP_EXT
-	  #test -c $SPECTRUM_DEV && rm $SPECTRUM_DEV
+	if egrep "^spc$SPECTRUM_SMP_EXT " /proc/modules >/dev/null ; then
+	  # test wheter it is still used
+	  if test "`lsof $SPECTRUM_DEV`x" = "x"; then
+		$MODPROBE -r ${SPECTRUM_NAME}${SPECTRUM_SMP_EXT}
+	  else
+	        echo "spectrum driver still in use (use lsof)"
+	  fi
 	fi
-	if $LSMOD | egrep "^pulseblaster ">/dev/null ; then
-	  $RMMOD pulseblaster
-	  test -c $PULSEBLASTER_DEV && rm $PULSEBLASTER_DEV
+	if egrep "^${PULSEBLASTER_NAME} " /proc/modules > /dev/null ; then
+	  # test wheter it is still used
+	  $MODPROBE -r ${PULSEBLASTER_NAME}
 	fi
        
 }
 
 status () {
-	if $LSMOD | egrep "^spc$SPECTRUM_SMP_EXT ">/dev/null ; then
+	if egrep "^${SPECTRUM_NAME}$SPECTRUM_SMP_EXT " /proc/modules > /dev/null ; then
 	   echo spectrum module loaded
 	else
 	   echo spectrum module not loaded
 	fi
-	if $LSMOD | egrep "^pulseblaster ">/dev/null ; then
+	if egrep "^${PULSEBLASTER_NAME} " /proc/modules > /dev/null ; then
 	   echo pulseblaster module loaded
 	else
 	   echo pulseblaster module not loaded
