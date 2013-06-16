3a665cf3b009a502f06e4f852e021173aa3181a7
markusro <markusro@b231d36d-50f4-0310-9822-a9ec2037227d>
2013-06-16 09:16:09 +0000

    prepare for wheezy

--- damaris/create_debs.sh
+++ damaris/create_debs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export SPC_SOURCE=/home/markusro/sources/spc_driver/micx_drv
 export DEBEMAIL="Markus.Rosenstihl@physik.tu-darmstadt.de"
 export DEBFULLNAME="Markus Rosenstihl"
@@ -11,14 +12,22 @@ dpkg-buildpackage -us -uc
 cd -
 # extract revision
 rev=$(svn info|grep Revision|cut -d" " -f2)
-newdir="$head/../dpkg-archive/$(date +%F)/$rev/"
-mkdir -p $newdir
+# extract debian release
+if [ $(cat /etc/debian_version) == "7.0" ]; then
+	release="wheezy"
+elif [ $(cat /etc/debian_version) == "6.0" ]; then
+	release="squeeze"
+else
+	release="none"
+fi
+newdir="$head/../dpkg-archive/$(date +%F)/$release/rev$rev/"
+mkdir -vp $newdir
 for deb in $(find $(pwd)/.. -regex ".*\(deb\|changes\|dsc\|tar\.gz\)" |grep -v archive)
 do
 	mv -v "$deb" "$newdir"
 done
 
 #ssh element sh /home/markusro/public_html/prepare.sh
-scp -r $newdir  element:/home/markusro/public_html/damaris-nightly
+scp -r $newdir  element:/home/markusro/public_html/damaris-nightly/$release
 
 
