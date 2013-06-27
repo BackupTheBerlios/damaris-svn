9785135dbe04367cd7090a155d992e1f2d21d587
markusro <markusro@b231d36d-50f4-0310-9822-a9ec2037227d>
2013-06-27 12:46:30 +0000

    new svn release

--- damaris/backends/drivers/SpinCore-PulseBlaster24Bit/SpinCore-PulseBlaster24Bit.cpp
+++ damaris/backends/drivers/SpinCore-PulseBlaster24Bit/SpinCore-PulseBlaster24Bit.cpp
@@ -32,48 +32,48 @@ PulseBlaster24BitCommand::PulseBlaster24BitCommand(const PulseBlaster24BitComman
 PulseBlaster24BitCommand::PulseBlaster24BitCommand(PulseBlaster24BitProgram& p): PulseBlasterCommand(p) {}
 
 PulseBlaster24BitCommand::PulseBlaster24BitCommand(PulseBlaster24BitProgram& p,
-						   int _ttls, double _length)
-  :PulseBlasterCommand(p, _ttls, _length)
+        int _ttls, double _length)
+    :PulseBlasterCommand(p, _ttls, _length)
 {}
 
 int PulseBlaster24BitCommand::write_to_file(FILE* out, size_t indent) const {
 
-  if (program==NULL) throw pulse_exception("PulseBlaster24Bit: Command not associated with Program");
-  fprintf(out,"%s<instruction ttls=\"0x%06x\" ",
-	  std::string(indent,' ').c_str(),ttls);
-  switch(instruction) {
-  case SpinCorePulseBlaster::LOOP:
-    fprintf(out,"inst=\"LOOP\" loops=\"%d\"",loop_count);
-    break;
-  case SpinCorePulseBlaster::LONG_DELAY:
-    fprintf(out,"inst=\"LONG_DELAY\" loops=\"%d\"",loop_count);
-    break;
-  case SpinCorePulseBlaster::BRANCH:
-    fprintf(out,"inst=\"BRANCH\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
-    break;
-  case SpinCorePulseBlaster::END_LOOP:
-    fprintf(out,"inst=\"END_LOOP\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
-    break;
-  case SpinCorePulseBlaster::JSR:
-    fprintf(out,"inst=\"JSR\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
-    break;
-  case SpinCorePulseBlaster::CONTINUE:
-    fprintf(out,"inst=\"CONTINUE\"");
-    break;
-  case SpinCorePulseBlaster::STOP:
-    fprintf(out,"inst=\"STOP\"");
-    break;
-  case SpinCorePulseBlaster::RTS:
-    fprintf(out,"inst=\"RTS\"");
-    break;
-  case SpinCorePulseBlaster::WAIT:
-    fprintf(out,"inst=\"WAIT\"");
-    break;
-  default:
-    fprintf(out,"inst=\"%d\" instdata=\"UNKNOWN\"",instruction);
-  }
-  fprintf(out," length=\"%g\"/>\n",(1.0*length)/program->internal_clock_freq);
-  return 1;
+    if (program==NULL) throw pulse_exception("PulseBlaster24Bit: Command not associated with Program");
+    fprintf(out,"%s<instruction ttls=\"0x%06x\" ",
+            std::string(indent,' ').c_str(),ttls);
+    switch(instruction) {
+    case SpinCorePulseBlaster::LOOP:
+        fprintf(out,"inst=\"LOOP\" loops=\"%d\"",loop_count);
+        break;
+    case SpinCorePulseBlaster::LONG_DELAY:
+        fprintf(out,"inst=\"LONG_DELAY\" loops=\"%d\"",loop_count);
+        break;
+    case SpinCorePulseBlaster::BRANCH:
+        fprintf(out,"inst=\"BRANCH\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
+        break;
+    case SpinCorePulseBlaster::END_LOOP:
+        fprintf(out,"inst=\"END_LOOP\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
+        break;
+    case SpinCorePulseBlaster::JSR:
+        fprintf(out,"inst=\"JSR\" address=\"%" SIZETPRINTFLETTER "\"",std::distance(program->begin(),jump));
+        break;
+    case SpinCorePulseBlaster::CONTINUE:
+        fprintf(out,"inst=\"CONTINUE\"");
+        break;
+    case SpinCorePulseBlaster::STOP:
+        fprintf(out,"inst=\"STOP\"");
+        break;
+    case SpinCorePulseBlaster::RTS:
+        fprintf(out,"inst=\"RTS\"");
+        break;
+    case SpinCorePulseBlaster::WAIT:
+        fprintf(out,"inst=\"WAIT\"");
+        break;
+    default:
+        fprintf(out,"inst=\"%d\" instdata=\"UNKNOWN\"",instruction);
+    }
+    fprintf(out," length=\"%g\"/>\n",(1.0*length)/program->internal_clock_freq);
+    return 1;
 }
 
 /*********************************************************************
@@ -83,9 +83,9 @@ int PulseBlaster24BitCommand::write_to_file(FILE* out, size_t indent) const {
 *********************************************************************/
 
 PulseBlaster24BitProgram::PulseBlaster24BitProgram(): PulseBlasterProgram() {
-  internal_clock_freq=100.0e6;
-  minimum_interval=9;
-  ttl_device_id=0;
+    internal_clock_freq=100.0e6;
+    minimum_interval=9;
+    ttl_device_id=0;
 }
 
 PulseBlaster24BitProgram::PulseBlaster24BitProgram(const SpinCorePulseBlaster24Bit& pb24) {
@@ -100,70 +100,74 @@ PulseBlaster24BitProgram::PulseBlaster24BitProgram(const PulseBlaster24BitProgra
     clear();
     const_iterator orig_i;
     for (orig_i=orig.begin(); orig_i!=orig.end(); ++orig_i) {
-	const PulseBlaster24BitCommand* the_command=dynamic_cast<const PulseBlaster24BitCommand*>(*orig_i);
-	if (the_command==NULL) {
-	    throw SpinCorePulseBlaster_error("wrong command class or NULL pointer found in program");
-	}
-	PulseBlaster24BitCommand* new_one=new PulseBlaster24BitCommand(*the_command);
-	new_one->program=this;
-	push_back(new_one);
+        const PulseBlaster24BitCommand* the_command=dynamic_cast<const PulseBlaster24BitCommand*>(*orig_i);
+        if (the_command==NULL) {
+            throw SpinCorePulseBlaster_error("wrong command class or NULL pointer found in program");
+        }
+        PulseBlaster24BitCommand* new_one=new PulseBlaster24BitCommand(*the_command);
+        new_one->program=this;
+        push_back(new_one);
     }
     // set correct references...
     orig_i=orig.begin();
-    for(iterator i=begin();i!=end();++i) {
-	(**i).program=this;
-	if ((**i).instruction==SpinCorePulseBlaster::END_LOOP || (**i).instruction==SpinCorePulseBlaster::BRANCH || (**i).instruction==SpinCorePulseBlaster::JSR) {
-	    (**i).jump=i;
-	    advance((**i).jump,distance(orig.begin(),(**orig_i).jump)-distance(orig.begin(),orig_i));
-	}
-	++orig_i;
+    for(iterator i=begin(); i!=end(); ++i) {
+        (**i).program=this;
+        if ((**i).instruction==SpinCorePulseBlaster::END_LOOP || (**i).instruction==SpinCorePulseBlaster::BRANCH || (**i).instruction==SpinCorePulseBlaster::JSR) {
+            (**i).jump=i;
+            advance((**i).jump,distance(orig.begin(),(**orig_i).jump)-distance(orig.begin(),orig_i));
+        }
+        ++orig_i;
     }
 }
 
 
 PulseBlasterCommand* PulseBlaster24BitProgram::create_command(const state& the_state) {
     if (typeid(the_state)==typeid(state_sequent)||typeid(the_state)==typeid(state_parallel))
-	throw pulse_exception("PulseBlasterCommands can only be created from states");
-    if (the_state.length < minimum_interval)
-	throw pulse_exception("PulseBlaster state too short: t<90ns");
+        throw pulse_exception("PulseBlasterCommands can only be created from states");
+    long unsigned int interval = ceil(the_state.length*internal_clock_freq);
+    if ( interval  < minimum_interval) {
+        fprintf(stderr,"PulseBlaster state too short (%lu < %lu ticks): %e\n", interval, \
+							minimum_interval, \
+							the_state.length);
+        throw pulse_exception("PulseBlaster state too short");
+    }
     // all states or-ed together
     int the_ttls=0;
-    for (state::const_iterator i=the_state.begin();i!=the_state.end();++i) {
-	const ttlout* to=dynamic_cast<const ttlout*>(*i);
-	if (to!=NULL && to->id==ttl_device_id) {
-	    the_ttls|=to->ttls.to_ulong();
-	}
+    for (state::const_iterator i=the_state.begin(); i!=the_state.end(); ++i) {
+        const ttlout* to=dynamic_cast<const ttlout*>(*i);
+        if (to!=NULL && to->id==ttl_device_id) {
+            the_ttls|=to->ttls.to_ulong();
+        }
     }
-    //fprintf(stderr, "state length=%g ttls=%x\n",the_state.length, the_ttls);
     return new PulseBlaster24BitCommand(*this, the_ttls, the_state.length);
 }
 
 PulseBlasterCommand* PulseBlaster24BitProgram::create_command(const PulseBlasterCommand* orig) {
     PulseBlaster24BitCommand* new_command=NULL;
     if (orig==NULL) {
-	new_command=new PulseBlaster24BitCommand();
-	new_command->program=this;
-	new_command->length=minimum_interval;
+        new_command=new PulseBlaster24BitCommand();
+        new_command->program=this;
+        new_command->length=minimum_interval;
     }
     else {
-	const PulseBlaster24BitCommand* command24Bit=dynamic_cast<const PulseBlaster24BitCommand*>(orig);
-	if (command24Bit==NULL) throw pulse_exception("wrong PulseBlasterCommand class");
-	new_command=new PulseBlaster24BitCommand(*command24Bit);
+        const PulseBlaster24BitCommand* command24Bit=dynamic_cast<const PulseBlaster24BitCommand*>(orig);
+        if (command24Bit==NULL) throw pulse_exception("wrong PulseBlasterCommand class");
+        new_command=new PulseBlaster24BitCommand(*command24Bit);
     }
     return new_command;
 }
 
 
 int PulseBlaster24BitProgram::write_to_file(FILE* out, size_t indent) const {
-  std::string indent_string(indent,' ');
-  fprintf(out,"%s<PulseBlaster24BitProgram>\n",indent_string.c_str());
+    std::string indent_string(indent,' ');
+    fprintf(out,"%s<PulseBlaster24BitProgram>\n",indent_string.c_str());
 
-  for(const_iterator i=begin();i!=end();++i) {
-    if (*i==NULL) throw SpinCorePulseBlaster_error("NULL pointer found in command list");
-    (**i).write_to_file(out, indent+2);
-  }
-  fprintf(out,"%s</PulseBlaster24BitProgram>\n",indent_string.c_str());
-  return 1;
+    for(const_iterator i=begin(); i!=end(); ++i) {
+        if (*i==NULL) throw SpinCorePulseBlaster_error("NULL pointer found in command list");
+        (**i).write_to_file(out, indent+2);
+    }
+    fprintf(out,"%s</PulseBlaster24BitProgram>\n",indent_string.c_str());
+    return 1;
 }
 
 
@@ -175,8 +179,8 @@ int PulseBlaster24BitProgram::write_to_file(FILE* out, size_t indent) const {
 
 void SpinCorePulseBlaster24Bit::write_command(unsigned char* data, int flags, opcode inst, unsigned int inst_data, size_t delay) {
     if (inst>8)
-	throw SpinCorePulseBlaster_error("instruction code not known");
-        
+        throw SpinCorePulseBlaster_error("instruction code not known");
+
     // Output, Control Word 1st Byte
     data[0]=(flags&0xff0000)>>16;
     // Output, Control Word 2nd Byte
@@ -202,7 +206,7 @@ void SpinCorePulseBlaster24Bit::write_command(unsigned char* data, int flags, op
        equal 0xff and delay >255. In this case reduce state for 10ns.
     */
     if (0 && data[9]==0xff && delay>0xff)
-      data[9]=0xfe;
+        data[9]=0xfe;
 }
 
 
@@ -210,48 +214,48 @@ void SpinCorePulseBlaster24Bit::write_command(unsigned char* data, int flags, op
 void SpinCorePulseBlaster24Bit::write_command(unsigned char* data, const PulseBlasterCommand& command) {
     const PulseBlaster24BitCommand* command24Bit=dynamic_cast<const PulseBlaster24BitCommand*>(&command);
     if (command24Bit==NULL)
-	throw SpinCorePulseBlaster_error("found wrong command class in PulseBlasterProgram");
+        throw SpinCorePulseBlaster_error("found wrong command class in PulseBlasterProgram");
 
     if (command24Bit->program==NULL) throw SpinCorePulseBlaster_error("Command not associated with Program");
     unsigned int inst_data=0;
     switch(command24Bit->instruction) {
-	case SpinCorePulseBlaster::CONTINUE:
-	case SpinCorePulseBlaster::STOP:
-	case SpinCorePulseBlaster::WAIT:
-	case SpinCorePulseBlaster::RTS:
-	    // no parameter
-	    break;
-	case SpinCorePulseBlaster::LOOP:
-	    inst_data=command24Bit->loop_count-1;
-	    break;
-	case SpinCorePulseBlaster::LONG_DELAY:
-	    inst_data=command24Bit->loop_count-2;
-	    break;
-	case SpinCorePulseBlaster::BRANCH:
-	case SpinCorePulseBlaster::END_LOOP:
-	case SpinCorePulseBlaster::JSR:
-	    inst_data=std::distance(command24Bit->program->begin(),command24Bit->jump);
-	    break;
-	default:
-	    throw SpinCorePulseBlaster_error("instruction code not known");
+    case SpinCorePulseBlaster::CONTINUE:
+    case SpinCorePulseBlaster::STOP:
+    case SpinCorePulseBlaster::WAIT:
+    case SpinCorePulseBlaster::RTS:
+        // no parameter
+        break;
+    case SpinCorePulseBlaster::LOOP:
+        inst_data=command24Bit->loop_count-1;
+        break;
+    case SpinCorePulseBlaster::LONG_DELAY:
+        inst_data=command24Bit->loop_count-2;
+        break;
+    case SpinCorePulseBlaster::BRANCH:
+    case SpinCorePulseBlaster::END_LOOP:
+    case SpinCorePulseBlaster::JSR:
+        inst_data=std::distance(command24Bit->program->begin(),command24Bit->jump);
+        break;
+    default:
+        throw SpinCorePulseBlaster_error("instruction code not known");
     }
     if (command24Bit->length<3)
-	    throw SpinCorePulseBlaster_error("delay length too small!");
+        throw SpinCorePulseBlaster_error("delay length too small!");
     unsigned int delay=(unsigned int)command24Bit->length-3;
     write_command(data,command24Bit->ttls,command24Bit->instruction,inst_data,delay);
 }
 
 void SpinCorePulseBlaster24Bit::write_to_device(const PulseBlaster24BitProgram& p) {
-  std::string program;
-  //Begin pulse program
-  for (PulseBlaster24BitProgram::const_iterator c=p.begin(); c!=p.end();++c) {
-    char command[10];
-    write_command((unsigned char*)command,**c);
-    program.append(command, (size_t)10);
-  }
-  set_program(program);
-  // End of programming registers and pulse program
-  set_initialized();
+    std::string program;
+    //Begin pulse program
+    for (PulseBlaster24BitProgram::const_iterator c=p.begin(); c!=p.end(); ++c) {
+        char command[10];
+        write_command((unsigned char*)command,**c);
+        program.append(command, (size_t)10);
+    }
+    set_program(program);
+    // End of programming registers and pulse program
+    set_initialized();
 }
 
 PulseBlasterProgram* SpinCorePulseBlaster24Bit::create_program(state& exp) {
@@ -264,7 +268,7 @@ PulseBlasterProgram* SpinCorePulseBlaster24Bit::create_program(state& exp) {
 void SpinCorePulseBlaster24Bit::run_pulse_program(const PulseBlasterProgram& p) {
     const PulseBlaster24BitProgram* prog=dynamic_cast<const PulseBlaster24BitProgram*>(&p);
     if (prog==NULL)
-	throw SpinCorePulseBlaster_error("found wrong program class in SpinCorePulseBlaster24Bit method");
+        throw SpinCorePulseBlaster_error("found wrong program class in SpinCorePulseBlaster24Bit method");
     write_to_device(*prog);
     start();
 }
--- damaris/backends/machines/general.cpp
+++ damaris/backends/machines/general.cpp
@@ -34,146 +34,147 @@
 
 class general_hardware: public hardware {
 
-  PTS* my_pts;
-  SpinCorePulseBlaster24Bit* my_pulseblaster;
-  SpectrumMI40xxSeries* my_adc;
+    PTS* my_pts;
+    SpinCorePulseBlaster24Bit* my_pulseblaster;
+    SpectrumMI40xxSeries* my_adc;
 
 public:
-  general_hardware(){
-      int SYSCONF=0;
-      int USRCONF=0;
-      GKeyFile* cfg_file;
-      GError *error=NULL;
-      char cfg_name[512];
-      const gchar* usr_configdir;
-      const gchar* const*  sys_configdirs;
-      sys_configdirs = g_get_system_config_dirs(); 
-      usr_configdir = g_get_user_config_dir();
-      cfg_file = g_key_file_new();
-      for (int i=0; i<100; i++) {
-        if (sys_configdirs[i]==NULL) break;
-        else {
-          snprintf(cfg_name, 512,"%s/damaris/backend.conf", sys_configdirs[i]);
-          fprintf(stdout, "reading backend configuration file (system: %i): %s...", i,cfg_name);
-          if (!g_key_file_load_from_file (cfg_file, cfg_name, G_KEY_FILE_NONE, &error)){
+    general_hardware() {
+        int SYSCONF=0;
+        int USRCONF=0;
+        GKeyFile* cfg_file;
+        GError *error=NULL;
+        char cfg_name[512];
+        const gchar* usr_configdir;
+        const gchar* const*  sys_configdirs;
+        sys_configdirs = g_get_system_config_dirs();
+        usr_configdir = g_get_user_config_dir();
+        cfg_file = g_key_file_new();
+        for (int i=0; i<100; i++) {
+            if (sys_configdirs[i]==NULL) break;
+            else {
+                snprintf(cfg_name, 512,"%s/damaris/backend.conf", sys_configdirs[i]);
+                fprintf(stdout, "reading backend configuration file (system: %i): %s...\n", i,cfg_name);
+                if (!g_key_file_load_from_file (cfg_file, cfg_name, G_KEY_FILE_NONE, &error)) {
+                    if (error->code != 4) {
+                        fprintf(stdout, "found!\n");
+                        g_log(G_LOG_DOMAIN, G_LOG_LEVEL_WARNING, error->message);
+                    }
+                    else
+                        printf("not found!\n");
+                }
+                else
+                    SYSCONF=1;
+                error=NULL; // reset error
+            }
+
+        }
+        snprintf(cfg_name, 512,"%s/damaris/backend.conf", usr_configdir);
+        fprintf(stdout, "reading backend configuration file (user): %s...\n", cfg_name);
+        if (!g_key_file_load_from_file (cfg_file, cfg_name, G_KEY_FILE_NONE, &error)) {
             if (error->code != 4) {
-              fprintf(stdout, "found!\n");
-              g_log(G_LOG_DOMAIN, G_LOG_LEVEL_WARNING, error->message);
+                fprintf(stdout, "found!\n");
+                g_log(G_LOG_DOMAIN, G_LOG_LEVEL_WARNING, error->message);
             }
             else
-              printf("not found!\n");
-          }
-          else
-            SYSCONF=1;
-          error=NULL; // reset error
+                printf("not found!\n");
+            error = NULL;
         }
-        
-      }
-      snprintf(cfg_name, 512,"%s/damaris/backend.conf", usr_configdir);
-      fprintf(stdout, "reading backend configuration file (user): %s...", cfg_name);
-      if (!g_key_file_load_from_file (cfg_file, cfg_name, G_KEY_FILE_NONE, &error)){
-        if (error->code != 4)
-          g_log(G_LOG_DOMAIN, G_LOG_LEVEL_WARNING, error->message);
         else
-          printf("not found!\n");
+            USRCONF=1;
+        if ( !(SYSCONF|USRCONF))
+            throw(core_exception("configuration failed!\n"));
+        printf("done!\n");
+        /* configure ADC card */
+        ttlout trigger;
+        trigger.id 		= g_key_file_get_integer(cfg_file, "ADC","id", &error);
+        if (error) g_error(error->message);
         error = NULL;
-      }
-      else
-        USRCONF=1;
-      if ( !(SYSCONF|USRCONF))
-        throw(core_exception("configuration failed!\n"));
-      printf("done!\n");
-      /* configure ADC card */
-      ttlout trigger;
-      trigger.id 		= g_key_file_get_integer(cfg_file, "ADC","id", &error);
-      if (error) g_error(error->message);
-      error = NULL;
-      //g_error(error->message);
-      trigger.ttls 		= 1<<g_key_file_get_integer(cfg_file, "ADC", "trigger_line", &error); 
-      if (error) g_error(error->message);
-      error = NULL;
-      int ext_reference_clock 	= (int) g_key_file_get_double(cfg_file, "ADC", "refclock", &error); // 50 MHz from PB24 SP17; defaults to 100MHz (PB24 SP 2)
-      if (error) g_error(error->message);
-      printf("reclock %i", ext_reference_clock);
-      error = NULL;
-      double impedance 		= g_key_file_get_double(cfg_file, "ADC", "impedance", &error); // Ohm ( or 50 Ohm)
-      if (error) g_error(error->message);
+        //g_error(error->message);
+        trigger.ttls 		= 1<<g_key_file_get_integer(cfg_file, "ADC", "trigger_line", &error);
+        if (error) g_error(error->message);
+        error = NULL;
+        int ext_reference_clock 	= (int) g_key_file_get_double(cfg_file, "ADC", "refclock", &error); // 50 MHz from PB24 SP17; defaults to 100MHz (PB24 SP 2)
+        if (error) g_error(error->message);
+        error = NULL;
+        double impedance 		= g_key_file_get_double(cfg_file, "ADC", "impedance", &error); // Ohm ( or 50 Ohm)
+        if (error) g_error(error->message);
 
-      error = NULL;
-      my_adc=new SpectrumMI40xxSeries(trigger, impedance, ext_reference_clock);
-      
-      /* configure PulseBlaster */
-      int pb_id = g_key_file_get_integer(cfg_file, "PB", "id", &error);
-      if (error) g_error(error->message);
-      error = NULL;
-      double pb_refclock = g_key_file_get_double(cfg_file, "PB", "refclock", &error);
-      if (error) g_error(error->message);
-      error = NULL;
-      int pb_sync_bit = g_key_file_get_integer(cfg_file, "PB", "sync_line", &error);
-      if (error) g_error(error->message);
-      error = NULL;
-      int pb_sync = 0;
-      if (pb_sync_bit != 128) pb_sync=1<<pb_sync_bit;
+        error = NULL;
+        my_adc=new SpectrumMI40xxSeries(trigger, impedance, ext_reference_clock);
 
-      my_pulseblaster=new SpinCorePulseBlaster24Bit(pb_id, pb_refclock, pb_sync);
+        /* configure PulseBlaster */
+        int pb_id = g_key_file_get_integer(cfg_file, "PB", "id", &error);
+        if (error) g_error(error->message);
+        error = NULL;
+        double pb_refclock = g_key_file_get_double(cfg_file, "PB", "refclock", &error);
+        if (error) g_error(error->message);
+        error = NULL;
+        int pb_sync_bit = g_key_file_get_integer(cfg_file, "PB", "sync_line", &error);
+        if (error) g_error(error->message);
+        error = NULL;
+        int pb_sync = 0;
+        if (pb_sync_bit != 128) pb_sync=1<<pb_sync_bit;
 
-      /* configure PTS */
-      int pts_id = g_key_file_get_integer(cfg_file, "PTS", "id", &error);
-      if (error) g_error(error->message);
-      error = NULL;
-      my_pts=new PTS_latched(pts_id);
-      // PTS 500 has 0.36 or 0.72 above 200MHz ;  PTS 310 has 0.225 degrees/step
-      my_pts->phase_step=g_key_file_get_double(cfg_file, "PTS", "phase_stepsize", &error);
-      if (error) g_error(error->message);
-      error = NULL;
+        my_pulseblaster=new SpinCorePulseBlaster24Bit(pb_id, pb_refclock, pb_sync);
 
-      // publish devices
-      the_pg=my_pulseblaster;
-      the_adc=my_adc;
-      the_fg=my_pts;
-  }
+        /* configure PTS */
+        int pts_id = g_key_file_get_integer(cfg_file, "PTS", "id", &error);
+        if (error) g_error(error->message);
+        error = NULL;
+        my_pts=new PTS_latched(pts_id);
+        // PTS 500 has 0.36 or 0.72 above 200MHz ;  PTS 310 has 0.225 degrees/step
+        my_pts->phase_step=g_key_file_get_double(cfg_file, "PTS", "phase_stepsize", &error);
+        if (error) g_error(error->message);
+        error = NULL;
 
-  result* experiment(const state& exp) {
-    result* r=NULL;
-    for(size_t tries=0; r==NULL && core::term_signal==0 &&  tries<102; ++tries) {
-      state* work_copy=exp.copy_flat();
-      if (work_copy==NULL) return new error_result(1,"could create work copy of experiment sequence");
-      try {
-	if (the_fg!=NULL)
-	  the_fg->set_frequency(*work_copy);
-	if (the_adc!=NULL)
-	  the_adc->set_daq(*work_copy);
-	// the pulse generator is necessary
-	my_pulseblaster->run_pulse_program_w_sync(*work_copy, my_adc->get_sample_clock_frequency());
-	// wait for pulse generator
-	the_pg->wait_till_end();
-	// after that, the result must be available
-	if (the_adc!=NULL)
-	  r=the_adc->get_samples();
-	else
-	  r=new adc_result(1,0,NULL);
-      }
-      catch (frequ_exception e) {
-	r=new error_result(1,"frequ_exception: "+e);
-      }
-      catch (ADC_exception e) {
-	if (e!="ran into timeout!" || tries>=100)
-	  r=new error_result(1,"ADC_exception: "+e);
-      }
-      catch (pulse_exception p) {
-	r=new error_result(1,"pulse_exception: "+p);
-      }
-      delete work_copy;
-      if (core::quit_signal!=0) break;
+        // publish devices
+        the_pg=my_pulseblaster;
+        the_adc=my_adc;
+        the_fg=my_pts;
     }
-    return r;
-  }
 
-  virtual ~general_hardware() {
-    if (the_adc!=NULL) delete the_adc;
-    if (the_fg!=NULL) delete the_fg;
-    if (the_pg!=NULL) delete the_pg;
-  }
+    result* experiment(const state& exp) {
+        result* r=NULL;
+        for(size_t tries=0; r==NULL && core::term_signal==0 &&  tries<102; ++tries) {
+            state* work_copy=exp.copy_flat();
+            if (work_copy==NULL) return new error_result(1,"could create work copy of experiment sequence");
+            try {
+                if (the_fg!=NULL)
+                    the_fg->set_frequency(*work_copy);
+                if (the_adc!=NULL)
+                    the_adc->set_daq(*work_copy);
+                // the pulse generator is necessary
+                my_pulseblaster->run_pulse_program_w_sync(*work_copy, my_adc->get_sample_clock_frequency());
+                // wait for pulse generator
+                the_pg->wait_till_end();
+                // after that, the result must be available
+                if (the_adc!=NULL)
+                    r=the_adc->get_samples();
+                else
+                    r=new adc_result(1,0,NULL);
+            }
+            catch (frequ_exception e) {
+                r=new error_result(1,"frequ_exception: "+e);
+            }
+            catch (ADC_exception e) {
+                if (e!="ran into timeout!" || tries>=100)
+                    r=new error_result(1,"ADC_exception: "+e);
+            }
+            catch (pulse_exception p) {
+                r=new error_result(1,"pulse_exception: "+p);
+            }
+            delete work_copy;
+            if (core::quit_signal!=0) break;
+        }
+        return r;
+    }
+
+    virtual ~general_hardware() {
+        if (the_adc!=NULL) delete the_adc;
+        if (the_fg!=NULL) delete the_fg;
+        if (the_pg!=NULL) delete the_pg;
+    }
 
 };
 
@@ -181,15 +182,15 @@ public:
    \brief brings standard core together with the general NMR hardware
 */
 class general_core: public core {
-  std::string the_name;
+    std::string the_name;
 public:
     general_core(const core_config& conf): core(conf) {
-	the_hardware=new general_hardware();
-	the_name="berta core";
-  }
-  virtual const std::string& core_name() const {
-  	return the_name;
-  }
+        the_hardware=new general_hardware();
+        the_name="berta core";
+    }
+    virtual const std::string& core_name() const {
+        return the_name;
+    }
 };
 
 /**
@@ -197,25 +198,25 @@ public:
  */
 
 int main(int argc, const char** argv) {
-  int return_result=0;
-  try {
-      core_config my_conf(argv, argc);
-      // setup input and output
-      general_core my_core(my_conf);
-      // start core application
-      my_core.run();
-  }
-  catch(ADC_exception ae) {
-    fprintf(stderr,"adc: %s\n",ae.c_str());
-    return_result=1;
-  }
-  catch(core_exception ce) {
-    fprintf(stderr,"core: %s\n",ce.c_str());
-    return_result=1;
-  }
-  catch(pulse_exception pe) {
-    fprintf(stderr,"pulse: %s\n",pe.c_str());
-    return_result=1;
-  }
-  return return_result;
+    int return_result=0;
+    try {
+        core_config my_conf(argv, argc);
+        // setup input and output
+        general_core my_core(my_conf);
+        // start core application
+        my_core.run();
+    }
+    catch(ADC_exception ae) {
+        fprintf(stderr,"adc: %s\n",ae.c_str());
+        return_result=1;
+    }
+    catch(core_exception ce) {
+        fprintf(stderr,"core: %s\n",ce.c_str());
+        return_result=1;
+    }
+    catch(pulse_exception pe) {
+        fprintf(stderr,"pulse: %s\n",pe.c_str());
+        return_result=1;
+    }
+    return return_result;
 }
